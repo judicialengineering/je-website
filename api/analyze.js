143a2fa30f862f7ba75fba73685236bde237a3bd
judicialengineering <judicialengineering@gmail.com>
2026-04-13 14:12:28 -0400

    analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 res.setHeader('Access-Control-Allow-Origin', '*');
 res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -28,4 +28,4 @@ return res.status(200).json(data);
 console.error('Handler error:', error);
 return res.status(500).json({ error: error.message });
 }
-}
+};
